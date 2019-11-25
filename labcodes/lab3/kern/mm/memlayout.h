4e7b37281d6681b6309c284752dac6dbd3cfebed
jack-lio <lio6218@163.com>
2019-11-26 02:56:19 +0800

    rr

--- labcodes/lab3/kern/mm/memlayout.h
+++ labcodes/lab3/kern/mm/memlayout.h
@@ -55,7 +55,7 @@
 /* All physical memory mapped at this address */
 #define KERNBASE            0xC0000000
 #define KMEMSIZE            0x38000000                  // the maximum amount of physical memory
-#define KERNTOP             (KERNBASE + KMEMSIZE)
+#define KERNTOP             (KERNBASE + KMEMSIZE)   
 
 /* *
  * Virtual page table. Entry PDX[VPT] in the PD (Page Directory) contains
@@ -107,7 +107,7 @@ struct Page {
 };
 
 /* Flags describing the status of a page frame */
-#define PG_reserved                 0       // if this bit=1: the Page is reserved for kernel, cannot be used in alloc/free_pages; otherwise, this bit=0 
+#define PG_reserved                 0       // if this bit=1: the Page is reserved for kernel, cannot be used in alloc/free_pages; otherwise, this bit=0
 #define PG_property                 1       // if this bit=1: the Page is the head page of a free memory block(contains some continuous_addrress pages), and can be used in alloc_pages; if this bit=0: if the Page is the the head page of a free memory block, then this Page and the memory block is alloced. Or this Page isn't the head page.
 
 #define SetPageReserved(page)       set_bit(PG_reserved, &((page)->flags))
@@ -130,4 +130,3 @@ typedef struct {
 #endif /* !__ASSEMBLER__ */
 
 #endif /* !__KERN_MM_MEMLAYOUT_H__ */
-
